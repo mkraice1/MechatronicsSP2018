f91fcd20aa05b9ec9dd25b46cacd549efda84afa
Yichuan Tang <tangyichuan18@gmail.com>
2018-02-07 14:15:43 -0500

    added Ping_Scheme

---
+++ Ping_Scheme.ino/Ping_Scheme.ino.ino
@@ -0,0 +1,46 @@
+// Connect Ping))) signal pin to Arduino digital 8
+int signal=8;
+int distance;
+unsigned long pulseduration=0;
+void setup()
+{
+ pinMode(signal, OUTPUT);
+ Serial.begin(9600);
+}
+void measureDistance()
+{
+ // set pin as output so we can send a pulse
+ pinMode(signal, OUTPUT);
+// set output to LOW
+ digitalWrite(signal, LOW);
+ delayMicroseconds(5);
+ 
+ // now send the 5uS pulse out to activate Ping)))
+ digitalWrite(signal, HIGH);
+ delayMicroseconds(5);
+ digitalWrite(signal, LOW);
+ 
+ // now we need to change the digital pin
+ // to input to read the incoming pulse
+ pinMode(signal, INPUT);
+ 
+ // finally, measure the length of the incoming pulse
+ pulseduration=pulseIn(signal, HIGH);
+}
+void loop()
+{
+ // get the raw measurement data from Ping)))
+ measureDistance();
+ 
+ // divide the pulse length by half
+ pulseduration=pulseduration/2; 
+ 
+ // now convert to centimetres. We're metric here people...
+ distance = int(pulseduration/29);
+ 
+ // Display on serial monitor
+ Serial.print("Distance - ");
+ Serial.print(distance);
+ Serial.println(" cm");
+ delay(500);
+}
